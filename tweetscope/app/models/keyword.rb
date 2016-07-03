368dfc2e9d8c778dc2d6b03f3953edaa759e01eb
kentah <kenthow@gmail.com>
2016-07-03 13:52:07 -0500

    Add the twitter gem

--- tweetscope/app/models/keyword.rb
+++ tweetscope/app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ActiveRecord::Base
+  def grab_twitts
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "Z4TSEYT4kEFNmFy2v38ybMmWp"
+      config.consumer_secret     = "foIrVzfXOAwn8SqEymHOjASeEDfl2IVRFzxVSFlmwjw3EJMVAc"
+      config.access_token        = "259067979-oHC7BRQ6wk0CGmqHEyR5affXfw6VzxSAtVvWNwjB"
+      config.access_token_secret = "rDqaAj6DVJ2FMWvTYXujrBfNQOovAZNKdFsoROgsfrCl3"
+    end
+
+    client.search(self.word, result_type: "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
 end
